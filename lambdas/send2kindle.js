dbbc498aa45f398e76223459b52152f23ad2a95c
valteriomon <102440417+valteriomon@users.noreply.github.com>
2022-11-14 09:55:12 -0300

    Update send2kindle.js

--- lambdas/send2kindle.js
+++ lambdas/send2kindle.js
@@ -1,54 +1,99 @@
-import { request } from 'https';
+import { request, get } from 'https';
 export { handle };
 
-const KINDLE_EMAIL = "", SENDER_EMAIL = "", ENDPOINT = "";
-
 async function handle (event, context, callback) {
-    const enc = encodeURIComponent;
-    const email = enc(KINDLE_EMAIL);
-    const from  = enc(SENDER_EMAIL);
-    const paramsObj = event.queryStringParameters;
-    let url, title;
+    const e = encodeURIComponent;
+    const email = e(process.env.TO_EMAIL), from  = e(process.env.FROM_EMAIL), paramsObj = event.queryStringParameters;
+    let url, title, hn;
+
     if(paramsObj && Object.keys(paramsObj).length) {
-        title = enc(paramsObj.title);
-        url = enc(paramsObj.url);
+        title = e(paramsObj.title);
+        url = e(paramsObj.url);
+        hn = e(paramsObj.hn);
     } else {
-        const body = JSON.parse(event.body);
-        const article = body.items[0];
-        title = enc(article.title);
-        url = enc(article.canonical[0].href);
+        const article = JSON.parse(event.body).items[0];
+        title = e(article.title);
+        url = e(article.canonical[0].href);
     }
 
+    // SendToKindle
     const opts = {
-        hostname: ENDPOINT,
+        hostname: process.env.P2K_URL,
         path: ('/send.php?context=send&url=' + url),
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
-    };
-    const postData = `email=${email}&from=${from}&title=${title}`;
-    const response = await new Promise((resolve, reject) => {
-        let dataString = '';    
+    },
+    postData = `email=${email}&from=${from}&title=${title}`;
+
+    const s2k = await new Promise((resolve) => {
         const req = request(opts, res => {
-            res.on('data', chunk => {
-                dataString += chunk;
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
             });
             res.on('end', () => {
                 resolve({
-                    statusCode: 200,
-                    body: dataString
+                    statusCode: res.statusCode,
+                    message: "Sent to kindle"
                 });
             });
             res.on('error', (e) => {
-                reject({
-                    statusCode: 500,
-                    body: 'Something went wrong!'
+                console.error(e);
+                resolve({
+                    statusCode: 400,
+                    message: 'Error: Send To Kindle response.'
                 });
+            });
+        });
+        req.on('error', (e) => {
+            console.error(e);
+            resolve({
+                statusCode: 400,
+                message: 'Error: Send To Kindle request.'
             });
         });
         req.write(postData);
         req.end();
     });
-    return response;
+
+    // SendToAppScript
+    let s2as = null;
+    if(s2k.statusCode == 200 && hn) {
+        s2as = await new Promise((resolve) => {
+            get(`${process.env.GAS_URL}?hn=${hn}&title=${title}`, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve({
+                        statusCode: res.statusCode,
+                        message: " and to AppScript!"
+                    });
+                });
+                res.on('error', (e) => {
+                    console.error(e);
+                    resolve({
+                        statusCode: 400,
+                        message: 'Error: Send To AppScript response.'
+                    });
+                });
+            }).on('error', (e) => {
+                console.error(e);
+                resolve({
+                    statusCode: 400,
+                    message: 'Error: Send To AppScript request.'
+                });
+            });
+        });
+    }
+
+    const statusCode = s2as ? s2as.statusCode : s2k.statusCode;
+    const message = `${s2k.message} ${s2as ? s2as.message : ""}`;
+    return {
+        statusCode: statusCode,
+        body: `(function(){alert("${message}")})()`
+    };
 };
