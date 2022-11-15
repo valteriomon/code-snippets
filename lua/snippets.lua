f79f1d534c602bc00fe85837aa8dd4fbf77ff9e7
valteriomon <102440417+valteriomon@users.noreply.github.com>
2022-11-15 19:04:31 -0300

    Create snippets.lua

---
+++ lua/snippets.lua
@@ -0,0 +1,11 @@
+"{c1b} " .. ((string.len('{c1t}') > 30) and (string.sub('{c1t}', 1, 30) .. "...") or '{c1t}')
+
+{dh23z}:{dmz}
+
+{bl}
+
+100*{dd}/{ddim}
+
+({ddw0}==0 and 7 or {ddw0})*100/7
+
+(({ddw0}==0 and 6 or {ddw0}-1)+{dtp})*100/7
