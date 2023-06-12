a01a777e2133d570c4e24b8bbafeb739918b7a18
valteriomon <102440417+valteriomon@users.noreply.github.com>
2023-06-12 11:32:41 -0300

    clean comments

--- rip/rip.py
+++ rip/rip.py
@@ -5,11 +5,6 @@ import json
 import subprocess
 import os
 from multiprocessing import Process
-import time
-import watchdog.events
-import watchdog.observers
-# mport Observer
-# from watchdog.events import FileSystemEventHandler
 
 lic = None
 master = None
@@ -47,8 +42,6 @@ def get_pssh(content):
 
 def read_dir(directory, episode):
     global lic, master, pssh
-    # for filename in os.listdir(directory):
-            # elif filename.endswith('.m3u8') or filename.endswith('.txt'):
     file_path = os.path.join(directory, episode)
     with open(file_path + " - license.txt", 'r') as file:
         content = file.read()
@@ -62,16 +55,6 @@ def read_dir(directory, episode):
         content = file.read()
         pssh = get_pssh(content)
 
-        # file_path = os.path.join(directory, filename)
-        # with open(file_path, 'r') as file:
-        #     content = file.read()
-        #     if filename.endswith('pssh.m3u8'):
-        #         pssh = get_pssh(content)
-        #     elif filename.endswith('master.txt'):
-        #         master = content
-        #     elif filename.endswith('txt'):
-        #         lic = content
-
 def remove_files(directory, episode):
     for filename in os.listdir(directory):
         if filename.startswith(episode) and (filename.endswith('.m3u8') or filename.endswith('.txt') or filename.endswith('.json')):
@@ -80,7 +63,6 @@ def remove_files(directory, episode):
             print(f"Removed file: {file_path}")
 
 def main():
-    # global episode
     directory_path = "."
     for filename in os.listdir(directory_path):
         if filename.endswith('.json'):
@@ -94,80 +76,3 @@ def main():
             subprocess.call(["powershell", "-c", "(New-Object Media.SoundPlayer 'C:\Windows\Media\chimes.wav').PlaySync()"])
 
 main()
-# class Watcher:
-#     # Set the directory on watch
-#     watchDirectory = "."
-
-#     def __init__(self):
-#         self.observer = watchdog.observers.Observer()
-
-
-#     def run(self):
-#         event_handler = Handler()
-#         self.observer.schedule(event_handler, self.watchDirectory, recursive = True)
-#         self.observer.start()
-#         try:
-#             while True:
-#                 time.sleep(5)
-#         except:
-#             self.observer.stop()
-#             print("Observer Stopped")
-
-#         self.observer.join()
-
-
-# class Handler(watchdog.events.PatternMatchingEventHandler):
-#     def __init__(self):
-#         watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*license.txt'],
-#                                                              ignore_directories=True, case_sensitive=False)
-
-#     @staticmethod
-#     def on_created(event):
-#         if event.is_directory:
-#             return None
-#         else:
-#             main()
-
-        # elif event.event_type == 'created':
-        #     # Event is created, you can process it now
-        #     print("Watchdog received created event - % s." % event.src_path)
-        # elif event.event_type == 'modified':
-        #     # Event is modified, you can process it now
-        #     print("Watchdog received modified event - % s." % event.src_path)
-
-# if __name__ == '__main__':
-#     watch = Watcher()
-#     watch.run()
-
-
-
-# class NewFileHandler(FileSystemEventHandler):
-#     def __init__(self):
-#         # Set the patterns for PatternMatchingEventHandler
-#         watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv'],
-#                                                              ignore_directories=True, case_sensitive=False)
-#     def on_created(self, event):
-#         if not event.is_directory and event.src_path.endswith('- license.txt'):
-#             print(f"New license file created: {event.src_path}")
-#             time.sleep(1)
-#             main()
-
-# if __name__ == "__main__":
-#     arg = False
-#     if len(sys.argv) > 1:
-#         arg = sys.argv[1]
-#     # else:
-#     if arg == "--m":
-#         directory_path = '.'  # Current directory
-#         event_handler = NewFileHandler()
-#         observer = Observer()
-#         observer.schedule(event_handler, directory_path, recursive=False)
-#         observer.start()
-#         try:
-#             while True:
-#                 time.sleep(1)
-#         except KeyboardInterrupt:
-#             observer.stop()
-#         observer.join()
-#     else:
-#         main()
